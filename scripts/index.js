3d61878630b55fff156d40726de7e9e56efe5969
TinaHoldcroft <54176460+TinaHoldcroft@users.noreply.github.com>
2020-10-30 15:01:58 +0100

    pop-up

--- scripts/index.js
+++ scripts/index.js
@@ -3,3 +3,8 @@ const btns = document.querySelectorAll('.btn');
 for (let btn of btns) {
     btn.onclick = () => btn.classList.toggle('active');
 }
+
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+  }
