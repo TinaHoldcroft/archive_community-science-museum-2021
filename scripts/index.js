6685b4005a97b0589df07dc55efaeb1bb25ad864
TinaHoldcroft <54176460+TinaHoldcroft@users.noreply.github.com>
2020-12-12 13:51:59 +0100

    hero image

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-const btns = document.querySelectorAll('.btn');
+const btns = document.querySelectorAll('.SoMe');
 for (let btn of btns) { btn.onclick = () => btn.classList.toggle('active'); }
 function myFunction() {
     var popup = document.getElementById("myPopup");
