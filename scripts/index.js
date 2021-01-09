23bb28fd1765c091f26df28d4e0ba8d00e0bd195
TinaHoldcroft <54176460+TinaHoldcroft@users.noreply.github.com>
2021-01-09 16:32:02 +0100

    message changed to alert pop-up

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,9 @@
 const btns = document.querySelectorAll('.message-popup');
-for (let btn of btns) { btn.onclick = () => btn.classList.toggle('--active'); }
-function myFunction() {
+
+for (let btn of btns) { 
+    btn.onclick = () => btn.classList.toggle('--active'); 
+}
+function popUpFunction() {
     var popup = document.getElementById("messagePopup");
-    popup.classList.toggle("--show"); }
+    popup.classList.toggle("--show"); 
+}
